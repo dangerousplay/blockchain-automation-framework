9ab472518fc4964bc1929359e134f549b575835e
dangerousplay <gm.davi.gm@live.com>
2020-10-11 17:44:19 -0300

    [ci skip] Pushing deployment files for namespace, service accounts and clusterrolebinding
    Signed-off-by: dangerousplay <gm.davi.gm@live.com>

--- tf/kubernetes.tf
+++ tf/kubernetes.tf
@@ -1,8 +1,8 @@
-resource "helm_release" "consul" {
-  name  = "consul"
-  chart = "hashicorp/consul"
-
-  values = [
-    "${file("./helm/consul-values.yml")}"
-  ]
-}
+//resource "helm_release" "consul" {
+//  name  = "consul"
+//  chart = "hashicorp/consul"
+//
+//  values = [
+//    "${file("./helm/consul-values.yml")}"
+//  ]
+//}
--- tf/vault.tf
+++ tf/vault.tf
@@ -1,6 +1,7 @@
 
 locals {
   VAULT_DEV_ROOT_TOKEN = "MYTOKEN"
+  VAULT_EXTERNAL_PORT = 8200
 }
 
 
@@ -81,11 +82,26 @@ locals {
 //  }
 //}
 
+//hashicorp/vault
+resource "docker_container" "vault" {
+  name  = "vault"
+  image = "vault:1.5.4"
+  env = ["VAULT_DEV_ROOT_TOKEN_ID=${local.VAULT_DEV_ROOT_TOKEN}"]
+
+  ports {
+    internal = 8200
+    external = local.VAULT_EXTERNAL_PORT
+  }
+}
 
 output "vault_root_token" {
   value = local.VAULT_DEV_ROOT_TOKEN
 }
 
 output "vault_host" {
-  value = "docker_container.vault.ip_address"
+  value = docker_container.vault.ip_address
+}
+
+output "vault_port" {
+  value = local.VAULT_EXTERNAL_PORT
 }
