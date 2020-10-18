5fd840941460aaaaf2986cb249348820dd5bfa88
dangerousplay <gm.davi.gm@live.com>
2020-10-18 16:17:12 -0300

    [ci skip] Pushing deployment files for namespace, service accounts and clusterrolebinding
    Signed-off-by: dangerousplay <gm.davi.gm@live.com>

--- tf/kubernetes.tf
+++ tf/kubernetes.tf
@@ -1,8 +1,20 @@
-//resource "helm_release" "consul" {
-//  name  = "consul"
-//  chart = "hashicorp/consul"
-//
-//  values = [
-//    "${file("./helm/consul-values.yml")}"
-//  ]
-//}
+
+resource "helm_repository" "prometheus" {
+  name = "prometheus-community"
+  url = "https://prometheus-community.github.io/helm-charts"
+}
+
+resource "helm_repository" "hashicorp" {
+  name = "hashicorp"
+  url = "https://helm.releases.hashicorp.com"
+}
+
+resource "helm_repository" "fluxcd" {
+  name = "fluxcd"
+  url = "https://charts.fluxcd.io"
+}
+
+resource "helm_repository" "stable" {
+  name = "stable"
+  url = "https://kubernetes-charts.storage.googleapis.com/"
+}
